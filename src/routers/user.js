40ed3e56f37db96e7c797369c6395bf4547697ba
Chris Hatch <siege.hatch@gmail.com>
2019-05-24 09:08:09 -0500

    Added GET /users/:id/avatar to serve user avatars.

--- src/routers/user.js
+++ src/routers/user.js
@@ -74,6 +74,21 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
     })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch(e) {
+        res.status(404).send()
+    }
+})
+
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body) 
     const allowedUpdates = ['name', 'email', 'password', 'age']
